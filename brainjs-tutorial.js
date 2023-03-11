d091d7f1e80734d74d6f92b5f8b9f2ed134be05d
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-11 16:34:19 -0500

    continue learning

--- brainjs-tutorial.js
+++ brainjs-tutorial.js
@@ -14,11 +14,11 @@ const trainingData = [
   { input: [1, 1], output: [0] },
 ];
 
-net.train(trainingData, {
-  // this allows us to see the error (how far off the net was):
-  log: (error) => console.log(error),
-  logPeriod: 100,
-});
+// net.train(trainingData, {
+//   // this allows us to see the error (how far off the net was):
+//   log: (error) => console.log(error),
+//   logPeriod: 100,
+// });
 
 // for a time, error may go down or may go up
 // eventually the net catches on and starts to accelerate it's ability to learn
@@ -44,3 +44,50 @@ console.log('[1, 0]:', `${output3}`); // 0.9336008429527283
 
 const output4 = net.run([1, 1]);
 console.log('[1, 1]:', `${output4}`); // 0.09363768994808197
+
+// NEURAL NET NOT LEARNING
+const network = new brain.NeuralNetwork({ hiddenLayers: [1] });
+// one hidden layer with one neuron
+
+const trainingData2 = [
+  { input: [0, 0], output: [0] },
+  { input: [0, 1], output: [1] },
+  { input: [1, 0], output: [1] },
+  { input: [1, 1], output: [0] },
+];
+
+// network.train(trainingData2, {
+//   log: (error) => console.log(error),
+// });
+
+// iterations: 20000, training error: 0.16941228433950378
+// hit 20,000 iterations without understanding the problem or learning how to solve it
+
+const network2 = new brain.NeuralNetwork({ hiddenLayers: [3] });
+// one hidden layer with one neuron
+
+// network2.train(trainingData2, {
+//   log: (error) => console.log(error),
+// });
+
+// iterations: 4320, training error: 0.005031979642962998
+// learn in relatively short period of time
+// took 4,320 iterations
+
+// can have MORE than 1 hidden layer
+
+const network3 = new brain.NeuralNetwork({ hiddenLayers: [2, 2] });
+// 2 hidden layers w/ 2 neurons
+// the more hidden layers, the longer it takes the neural network to train
+
+network3.train(trainingData2, {
+  log: (error) => console.log(error),
+});
+
+// iterations: 20000, training error: 0.25450283011807695
+// hit 20,000 iterations w/o fully training
+
+// can use the hidden layers as a funnel
+// if you hve 20 inputs, you can have a hidden layer with 15 layers, then another with 10 layers, then 2 output layers
+
+//
---
+++ objectsNeuralNet.js
@@ -0,0 +1,102 @@
+import brain from 'brain.js';
+
+// input: { red, green, blue }
+// output: { light, neural, dark }
+
+// in brain.js you don't have to define all the properties
+// if they're missing, it uses a 0
+
+const colors = [
+  { green: 0.2, blue: 0.4 }, // red is missing, so it will be 0
+  { green: 0.4, blue: 0.6 },
+  { red: 0.2, green: 0.8, blue: 0.8 },
+  { green: 1, blue: 1 },
+  { red: 0.8, green: 1, blue: 1 },
+  { red: 1, green: 1, blue: 1 },
+  { red: 1, green: 0.8, blue: 0.8 },
+  { red: 1, green: 0.6, blue: 0.6 },
+  { red: 1, green: 0.4, blue: 0.4 },
+  { red: 1, green: 0.31, blue: 0.31 },
+  { red: 0.8 },
+  { red: 0.6, green: 0.2, blue: 0.2 },
+];
+
+const brightnesses = [
+  { dark: 0.8 },
+  { neutral: 0.8 },
+  { light: 0.7 },
+  { light: 0.8 },
+  { light: 0.9 },
+  { light: 1 },
+  { light: 0.8 },
+  { neutral: 0.7, light: 0.5 },
+  { dark: 0.5, neutral: 0.5 },
+  { dark: 0.6, neutral: 0.3 },
+  { dark: 0.85 },
+  { dark: 0.9 },
+];
+
+// create training data
+
+const trainingData = [];
+
+for (let i = 0; i < colors.length; i++) {
+  trainingData.push({
+    input: colors[i],
+    output: brightnesses[i],
+  });
+}
+
+// train neural net
+
+const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
+
+// log out what happens at very end (vs what happens at each )
+const stats = net.train(trainingData);
+
+console.log(stats);
+// { error: 0.004997083827757059, iterations: 1428 }
+// learned it pretty fast
+
+// see what neural net outputs:
+
+console.log(
+  net.run({
+    red: 0.9,
+  })
+);
+
+/*
+{
+  dark: 0.9445916414260864,
+  neutral: 0.028699304908514023,
+  light: 0.0013907894026488066
+} 
+*/
+
+// gives us that red is dark (when at 0.9)
+
+// bonus
+// what if we want to revert the problem?
+// asking the neural net for colors rather than classifying their brightness
+// change input and outputs?
+// inputs: { light, neutral, dark }
+// output: { red, green, blue }
+
+const invertedTrainingData = [];
+
+for (let i = 0; i < colors.length; i++) {
+  invertedTrainingData.push({
+    input: brightnesses[i],
+    output: colors[i],
+  });
+}
+
+const invertedNet = new brain.NeuralNetwork({ hiddenLayers: [3] });
+
+const invertedStats = invertedNet.train(invertedTrainingData);
+
+console.log(invertedStats);
+// { error: 0.025163172557859204, iterations: 20000 }
+// didn't do a great job at learning the problem
+// when you flip a neural net, you're kind of generating values that can be useful in predictions
