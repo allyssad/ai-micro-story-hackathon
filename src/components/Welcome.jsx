4b27015ec4abc53fea8456872d8b8fa2467fe02a
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-12 18:23:41 -0400

    add openai api calls

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,14 +1,71 @@
 import { useState } from 'react';
 import '../css/main.css';
+import { Configuration, OpenAIApi } from 'openai';
+
+// flat art, surrealistic art, naturalism,
 
 const Welcome = () => {
   const [name, setName] = useState('');
   const [topic, setTopic] = useState('');
   const [style, setStyle] = useState('');
   const [story, setStory] = useState('');
-  const [image, setImage] = useState('');
+  const [imageSrc, setImageSrc] = useState('');
+  const [tldr, setTldr] = useState('');
 
-  const callOpenAIAPI = (evt) => {
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    const response = await openai.createImage({
+      prompt: `${tldr || story} in the style of ${
+        style || "children's book illustration"
+      } art`,
+      n: 1,
+      size: '1024x1024',
+    });
+
+    setImageSrc(response.data.data[0].url);
+  };
+
+  const generateShortStory = async () => {
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `Topic: Unicorn\nTwo-Sentence Happy Story: Once upon a time, there was a happy unicorn named Sparkles who lived in a magical meadow filled with rainbows and sunshine. Sparkles spent her days frolicking through the meadow, spreading joy and laughter wherever she went.\n    \nTopic: ${
+        name ? name + ' ' + topic : topic
+      }\nTwo-Sentence Happy Story: .`,
+      temperature: 0.8,
+      max_tokens: 60,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    const generatedStory = response.data.choices[0].text;
+    setStory(generatedStory);
+    console.log(generatedStory);
+  };
+
+  const generateSummary = async () => {
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${story}\n\nTl;dr`,
+      temperature: 0.7,
+      max_tokens: 30,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 1,
+    });
+
+    let generatedSummary = response.data.choices[0].text.trim();
+    if (generatedSummary[0] === ':' || generatedSummary[0] === '-')
+      generatedSummary = generatedSummary.slice(1);
+    console.log(generatedSummary);
+    setTldr(generatedSummary);
+  };
+
+  const callOpenAIAPI = async (evt) => {
     evt.preventDefault();
     console.log('calling OpenAI API');
     console.log('NAME:', name);
@@ -23,7 +80,7 @@ const Welcome = () => {
   return (
     <>
       <h1>AI Micro Story Hour</h1>
-      <form onSubmit={callOpenAIAPI}>
+      <form onSubmit={generateShortStory}>
         <label htmlFor='name'>Enter a name:</label>
         <input
           type='text'
@@ -60,7 +117,6 @@ const Welcome = () => {
             <option value={"children's book illustration"}>Illustration</option>
             <option value={'pixel art'}>Pixel Art</option>
             <option value={'photorealistic'}>Photorealistic</option>
-            <option value={'comic book'}>Comic Book</option>
           </select>
         </div>
         <button type='submit'>Go!</button>
@@ -69,8 +125,27 @@ const Welcome = () => {
         <h2>RESULTS:</h2>
       </div>
       <div> {story.length ? <p>{story}</p> : 'AI Micro Story Holder...'}</div>
-      <div>{image.length ? <p>{image}</p> : 'AI Image Holder...'}</div>
+      <div>
+        {imageSrc.length ? (
+          <>
+            <img src={imageSrc}></img>
+            <p>{tldr}</p>
+            <p>In the style of: {style ? style : 'illustration'}</p>
+          </>
+        ) : (
+          'AI Image Holder...'
+        )}
+      </div>
       <button onSubmit={saveHandler}>SAVE</button>
+      <div>
+        <button onClick={generateImage}>Image</button>
+      </div>
+      <div>
+        <button onClick={generateShortStory}>Story</button>
+      </div>
+      <div>
+        <button onClick={generateSummary}>Summary</button>
+      </div>
     </>
   );
 };
