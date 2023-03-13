9bfed0dd98c404974e89176963af49390d3f221e
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-03-13 10:42:25 -0400

    add style and save seed

---
+++ seed.js
@@ -0,0 +1,71 @@
+[
+  {
+    name: 'State',
+    value: 'Celeste',
+    subHooks: [],
+    hookSource: {
+      lineNumber: 23,
+      functionName: 'Home',
+      fileName: 'http://localhost:5173/src/components/Home.jsx?t=1678715109601',
+      columnNumber: 27,
+    },
+  },
+  {
+    name: 'State',
+    value: 'space',
+    subHooks: [],
+    hookSource: {
+      lineNumber: 24,
+      functionName: 'Home',
+      fileName: 'http://localhost:5173/src/components/Home.jsx?t=1678715109601',
+      columnNumber: 29,
+    },
+  },
+  {
+    name: 'State',
+    value: 'watercolor',
+    subHooks: [],
+    hookSource: {
+      lineNumber: 25,
+      functionName: 'Home',
+      fileName: 'http://localhost:5173/src/components/Home.jsx?t=1678715109601',
+      columnNumber: 29,
+    },
+  },
+  {
+    name: 'State',
+    value:
+      'Celeste was an adventurous space traveler who roamed the galaxies, finding joy in exploring new worlds and mysterious alien creatures. On every journey, Celeste made friends, learned new cultures and even brought a little bit of stardust back home with her.',
+    subHooks: [],
+    hookSource: {
+      lineNumber: 26,
+      functionName: 'Home',
+      fileName: 'http://localhost:5173/src/components/Home.jsx?t=1678715109601',
+      columnNumber: 29,
+    },
+  },
+  {
+    name: 'State',
+    value:
+      'https://oaidalleapiprodscus.blob.core.windows.net/private/org-dShBvP5zA74G8md7lOB0HOIs/user-3uZYY0AZEc7s57tNY6TulIYd/img-rK7T1sTnXkaI0Rk1FQB2s05B.png?st=2023-03-13T13%3A08%3A23Z&se=2023-03-13T15%3A08%3A23Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-13T08%3A13%3A24Z&ske=2023-03-14T08%3A13%3A24Z&sks=b&skv=2021-08-06&sig=UaFT0sLpOYgZtmNdoe%2Bavt3u4yExb5Y7TAz9v0QTU78%3D',
+    subHooks: [],
+    hookSource: {
+      lineNumber: 27,
+      functionName: 'Home',
+      fileName: 'http://localhost:5173/src/components/Home.jsx?t=1678715109601',
+      columnNumber: 35,
+    },
+  },
+  {
+    name: 'State',
+    value:
+      ' Celeste was an adventurous space traveler who explored new worlds and made friends, learning about different cultures and bringing stardust home with her.',
+    subHooks: [],
+    hookSource: {
+      lineNumber: 28,
+      functionName: 'Home',
+      fileName: 'http://localhost:5173/src/components/Home.jsx?t=1678715109601',
+      columnNumber: 27,
+    },
+  },
+];
