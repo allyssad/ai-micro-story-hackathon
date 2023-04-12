6e8dc270f55682f350f2f2c591fc9ff0cf485afe
Allyssa Deorocki <allyssadeorocki@gmail.com>
2023-04-12 13:17:33 -0400

    integrate tailwind into file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home, NavBar } from './components/index';
+import './index.css';
 
 const App = () => {
   return (
-    <>
+    <div>
       <NavBar />
 
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
-    </>
+    </div>
   );
 };
 
